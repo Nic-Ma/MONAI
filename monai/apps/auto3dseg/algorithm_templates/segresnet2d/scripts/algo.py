e77eecb6c0fb8e402c72c4220fe9c7da8594d232
Nic Ma <nma@nvidia.com>
2022-08-30 16:29:19 +0800

    [DLMED] support customized algo
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- monai/apps/auto3dseg/algorithm_templates/segresnet2d/scripts/algo.py
+++ monai/apps/auto3dseg/algorithm_templates/segresnet2d/scripts/algo.py
@@ -11,111 +11,14 @@
 
 import os
 import shutil
-from copy import copy, deepcopy
+from copy import deepcopy
 from glob import glob
 from typing import Sequence, Union
 
-from monai.auto3dseg.algo_gen import Algo, AlgoGen
-from monai.bundle.config_parser import ConfigParser
-from monai.utils import ensure_tuple
-
-__all__ = ["BundleAlgo", "BundleGen", "SegresnetAlgo", "DintsAlgo"]
+from monai.apps.auto3dseg import BundleAlgo
 
 
-class BundleAlgo(Algo):
-    """
-    An algorithm represented by a set of bundle configurations and scripts.
-
-    ``BundleAlgo.cfg`` is a ``monai.bundle.ConfigParser`` instance.
-
-    ..code-block:: python
-
-        import glob
-
-        from monai.apps.auto3dseg import BundleAlgo
-
-        algo = BundleAlgo(
-            template_configs=../algorithms/templates/segresnet2d/configs,
-            scripts_path="../algorithms/templates/segresnet2d/scripts")
-        algo.set_data_stats("../algorithms/data_stats.yaml")
-        # algo.set_data_list("../data_list.json")
-        algo.export_to_disk(".", algo_name="segresnet2d_1")
-
-    """
-
-    def __init__(self, template_configs=None, scripts_path=None, meta_data_filename=None, parser_args=None):
-        if os.path.isdir(template_configs):
-            self.template_configs = []
-            for ext in ("json", "yaml"):
-                self.template_configs += glob(os.path.join(template_configs, f"*.{ext}"))
-        else:
-            self.template_configs = template_configs
-        self.meta_data_filename = meta_data_filename
-        self.cfg = ConfigParser(globals=False)  # TODO: define root folder (variable)?
-        if self.template_configs is not None:
-            self.load_templates(self.template_configs, meta_data_filename, parser_args)
-
-        self.scripts_path = scripts_path
-        self.data_stats_filename = None
-        self.data_list_file = None
-        self.output_path = None
-        self.name = None
-
-    def load_templates(self, config_files, metadata_file=None, parser_args=None):
-        parser_args = parser_args or {}
-        self.cfg.read_config(config_files, **parser_args)
-        if metadata_file is not None:
-            self.cfg.read_meta(metadata_file)
-
-    def set_data_stats(self, data_stats_filename):
-        self.data_stats_filename = data_stats_filename
-
-    def set_data_source(self, data_list_file):
-        self.data_list_file = data_list_file
-
-    def fill_template_config(self, data_stats_filename):
-        # self.algorithm_dir = os.path.join(self.output_path, "segresnet2d")
-        # self.inference_script = "scripts/infer.py"
-        pass
-
-    def export_to_disk(self, output_path, algo_name, **kwargs):
-        self.fill_template_config(self.data_stats_filename)
-        write_path = os.path.join(output_path, algo_name)
-        self.cfg["bundle_root"] = write_path
-        os.makedirs(write_path, exist_ok=True)
-        # handling scripts files
-        output_scripts_path = os.path.join(write_path, "scripts")
-        if os.path.exists(output_scripts_path):
-            shutil.rmtree(output_scripts_path)
-        if self.scripts_path is not None and os.path.exists(self.scripts_path):
-            shutil.copytree(self.scripts_path, output_scripts_path)
-        # handling config files
-        output_config_path = os.path.join(write_path, "configs")
-        if os.path.exists(output_config_path):
-            shutil.rmtree(output_config_path)
-        os.makedirs(output_config_path, exist_ok=True)
-        output_config_file = os.path.join(output_config_path, "algo_config.yaml")
-        ConfigParser.export_config_file(self.cfg.config, output_config_file, fmt="yaml", default_flow_style=None)
-        with open(output_config_file, "r+") as f:
-            lines = f.readlines()
-            f.seek(0)
-            f.write(f"# Generated automatically by `{__name__}`\n")
-            f.write("# For more information please visit: https://docs.monai.io/\n\n")
-            for item in ensure_tuple(self.template_configs):
-                f.write(f"# source file: {item}\n")
-            f.write("\n\n")
-            f.writelines(lines)
-        print(write_path)
-        self.output_path = write_path
-
-    def train(self):
-        pass
-
-    def get_score(self, *args, **kwargs):
-        pass
-
-
-class SegresnetAlgo(BundleAlgo):
+class Segresnet2DAlgo(BundleAlgo):
     def fill_template_config(self, data_stats_filename):
         if data_stats_filename is None or not os.path.exists(str(data_stats_filename)):
             return
@@ -211,9 +114,7 @@ class BundleGen(AlgoGen):
     def __init__(self, algos=None, data_stats_filename=None, data_lists_filename=None):
         self.algos = []
         if algos is None:
-            algos = copy(default_algos)
-        if isinstance(algos, dict):
-            for algo_name, algo_params in algos.items():
+            for algo_name, algo_params in default_algos.items():
                 self.algos.append(ConfigParser(algo_params).get_parsed_content())
                 self.algos[-1].name = algo_name
         else:
